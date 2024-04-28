58b9ec2e07ad611a414c337955c7d57513fa63a9
CallmeDante <157425497+Bringmedark@users.noreply.github.com>
2024-04-28 10:33:33 +0200

    Update 24_NSI_37.py
    #2024 sujet 37 Mehdi

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_37.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_37.py
@@ -1,15 +1,28 @@
+#2024 sujet 37
+
+#exercice 1
+def moyenne(tab):
+    if len(tab) == 0:
+        return "Le tableau est vide"
+    else:
+        somme = 0
+        for element in tab:
+            somme += element
+        return somme / len(tab)
+
+
+#exercice 2
 def tri(tab):
     '''tab est un tableau d'entiers contenant des 0 et des 1.
     La fonction trie ce tableau en plaçant tous les 0 à gauche'''
-    i = ... # premier indice de la zone non triée 
-    j = ... # dernier indice de la zone non triée 
+    i = 0  # premier indice de la zone non triée
+    j = len(tab) - 1  # dernier indice de la zone non triée
     while i < j:
         if tab[i] == 0:
-            i = ... 
+            i += 1
         else:
-            valeur = ... 
-            tab[j] = ... 
-            ...
-            j = ... 
-
+            valeur = tab[j]
+            tab[j] = tab[i]
+            tab[i] = valeur
+            j -= 1
 
